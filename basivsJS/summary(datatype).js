1c8812aac3284a15bf407f0857bc2753a598163f
Jatinpundir11 <xboxjatinrana@gmail.com>
2025-08-23 12:58:48 +0530

    Update summary(datatype).js

--- basivsJS/summary(datatype).js
+++ basivsJS/summary(datatype).js
@@ -1,5 +1,5 @@
 //there are two types of datatype:
-//primitive and non primitive on the bases on how data is stored and access in the memory
+//primitive and non primitive on the bases on how data is stored and access in the memory(stack and heap cocept)
 //primitive---here the value is provided not refernce and change in var changes in copy
 //////7 types: 1)string 2)number 3)undefined 4)boolean 5)null 6)symbol 7)bigint 
 //non primitive--also reference type---reference directly allocated in memory 1)array 2)objects 3)functions
